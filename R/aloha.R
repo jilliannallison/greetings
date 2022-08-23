35f7b740a28251095f6fa15f9478f7cf669e4c0a
jilliannallison <jilliannallison@ucsb.edu>
2022-08-23 16:06:41 -0700

    fixed it

--- R/aloha.R
+++ R/aloha.R
@@ -10,23 +10,20 @@
 #' @examples say_aloha("Allison")
 say_aloha <- function(name, color) {
 
-<<<<<<< HEAD
-  message <- paste("i love u,",
-=======
   message <- paste("ur poopy,",
->>>>>>> 15ecdc07a1a53bea1ebfdad99dbc91d9b1af1606
                    name,
                    emo::ji("palm_tree"),
                    emo::ji("sunny"),
                    emo::ji("ocean"))
 
   cat(crayon::bgCyan(message))
-=======
+
   if (color == "blue"){
     cat(crayon::bgBlue(message))
   }
   else if (color == "green"){
     cat(crayon::bgGreen(message))
   }
+}
 
 
