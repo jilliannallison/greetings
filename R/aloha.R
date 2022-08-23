5c0380eb85300b726430fbd9a3d8dcf8050197c0
erica-bishop <erica.nicole.bishop@gmail.com>
2022-08-23 16:02:18 -0700

    changed line 13

--- R/aloha.R
+++ R/aloha.R
@@ -10,7 +10,7 @@
 #' @examples say_aloha("Allison")
 say_aloha <- function(name, color) {
 
-  message <- paste("I hate you,",
+  message <- paste("ur poopy,",
                    name,
                    emo::ji("palm_tree"),
                    emo::ji("sunny"),
